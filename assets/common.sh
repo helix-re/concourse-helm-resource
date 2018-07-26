4138b6d321a4b82e5c3f9493710ac012058bc2e7
David O'Dell <dave@helix.re>
2018-07-26 11:22:41 -0700

    added date as first line of output

--- assets/common.sh
+++ assets/common.sh
@@ -8,8 +8,7 @@ info() { printf "${c_blue}%b${c_reset}" "$*\n"; }
 setup_kubernetes() {
     payload=$1
     source=$2
-    
-    ls -la /root/
+
     mkdir -p /root/.kube
     gcloud_auth=$(jq -r '.source.gcloud_auth // ""' < $payload)
     kubeconfig=$(jq -r '.source.kubeconfig // ""' < $payload)
@@ -64,6 +63,7 @@ wait_for_service_up() {
 }
 
 setup_resource() {
+    TZ=America/Los_Angeles date
     echo "Initializing kubectl..."
     setup_kubernetes $1 $2
     echo "Initializing helm..."
